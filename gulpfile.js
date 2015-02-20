7e50c6509bcbc230e594a562fc7e684ccb0e77dd
jatamayo <jalejandrotamayo@hotmail.com>
2015-02-20 13:20:30 -0600

    prototipo terminado

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var w3cjs = require('gulp-w3cjs');
 var csslint = require('gulp-csslint');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 gulp.task('htmllint', function () {
     gulp.src('*.html')
@@ -20,6 +22,24 @@ gulp.task('lint', function() {
   return gulp.src('js/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
+});
+
+// Watch Files For Changes & Reload
+gulp.task('serve', function () {
+browserSync({
+ notify: false,
+ // Customize the BrowserSync console logging prefix
+ logPrefix: 'WSK',
+ // Run as an https by uncommenting 'https: true'
+ // Note: this uses an unsigned certificate which on first access
+ //       will present a certificate warning in the browser.
+ // https: true,
+ server: ['.tmp', '.']
+});gulp.watch(['*.html'], reload);
+gulp.watch(['templates/**/*.html'], reload);
+gulp.watch(['styles/**/*.{scss,css}'], reload);
+gulp.watch(['js/**/*.js'], ['jshint']);
+gulp.watch(['img/**/*'], reload);
 });
 
 gulp.task('default', function() {
